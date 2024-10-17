6e440a69f85df6f44221bf5b6c9e92f423221b1e
movva-gpu <danyellastrikann@duck.com>
2024-10-17 10:09:08 +0200

    adds checsum check in the download script

--- scripts/get_consolidated_data.sh
+++ scripts/get_consolidated_data.sh
@@ -8,9 +8,18 @@ if [ "$(basename $PWD)" = "scripts" ]; then
     cd ..
 fi
 
+SHA1SUM="2bbb98714cfd502cdef6de61b2c474aea0e4df75"
+
 mkdir -p data
-echo "data from www.data.gouv.fr, last consolidated the 2024-10-17 (file generated from $PWD/get_consolidated_data.sh)" > data/consolidation.geo.json.info
 curl -L https://www.data.gouv.fr/fr/datasets/r/7eee8f09-5d1b-4f48-a304-5e99e8da1e26 > data/consolidation.geo.json
+
+if [ ! "$(sha1sum data/consolidation.geo.json | cut -d' ' -f1)" = "$SHA1SUM" ]; then
+    echo "Error: Checksum of download file isn't correct."
+    echo "Exiting..."
+    exit 1
+fi
+
+echo "data from www.data.gouv.fr, last consolidated the 2024-10-17 (file generated from $PWD/get_consolidated_data.sh)" > data/consolidation.geo.json.info
 
 if [ ! "$PWD" = "$O_PWD" ]; then
     cd $PWD
