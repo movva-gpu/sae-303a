af2a06e5fed67507d49a57c390552225930b33a9
Charlie-git-hub <charlieemail@proton.me>
2024-10-17 10:21:11 +0200

    correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,112 +1,170 @@
-const chart1 = document.getElementById('chart1');
-const chart2 = document.getElementById('chart2');
-const chart3 = document.getElementById('chart3');
-const chart4 = document.getElementById('chart4');
-const chart5 = document.getElementById('chart5');
-const chart6 = document.getElementById('chart6');
+chart1 = document.getElementById('chart1');
+chart2 = document.getElementById('chart2');
+chart3 = document.getElementById('chart3');
+chart4 = document.getElementById('chart4');
+chart5 = document.getElementById('chart5');
+chart6 = document.getElementById('chart6');
 
-Plotly.newPlot( chart1, [{
-
-y:['voiture', 'bus', 'train', 'avion', 'bateau', 'moto', 'vélo', 'trotinette'], 
-
-x: [90, 40, 60, 80, 75, 92, 87, 73],
-
-type: 'bar',
-
-orientation: 'h'}]);
-
+Plotly.newPlot('chart1', [{
+    x: [60, 2, 7, 2, 1, 26],
+    type: 'bar',
+    orientation: 'h',
+    y: [
+        'Voiture', 
+        'Bus', 
+        'Train', 
+        'Avion', 
+        'Bateau', 
+        'Moto', 
+        'Transport routier de marchandises'
+    ]
+   
+}], {
+    xaxis: {
+        range: [0, 100],  
+        title: 'Pourcentage (%)'
+    },
+    title: 'Répartition des types de transports'
+});
 Plotly.newPlot( chart2, [{
-
     values: [11, 14, 43, 13, 7, 12],
+    type: 'pie',
+    labels: [
+        'Autres secteurs', 
+        'Residentiel', 
+        'Industrie et construction', 
+        'Energie hors électricité', 
+        'Production d\'électricité'],
 
-    labels: ['autres secteurs', 'residentiel', 'industrie et construction', 'énergie hors électricité', 'production d\'électricité'],
-
-type: 'pie',
 }]);
 
 Plotly.newPlot( chart3, [{
-
-    values: [19, 26, 55],
-
-    labels: ['diesel thermique', 'essence', 'diesel hybride non rechargeable', 'essence', 'essence hybride non recheargeable'],
-type: 'pie',
+    values: [50.7, 0.4, 40.6, 3.9, 2.2, 1.5, 0.7],
+    type: 'pie',
+    labels: [
+        'Diesel thermique', 
+        'Essence', 
+        'Diesel hybride non rechargeable', 
+        'Essence', 
+        'Essence hybride non recheargeable', 
+        'Electricité et hydrogène', 
+        'Hybride rechargeable', 
+        'Bicarburation essence-gpl'
+    ],
 }]);
 
-Plotly.newPlot('chart4',
-  [
-    {
-      type: 'scattergeo',
-      mode: 'markers',
-      locations: ['FRA', 'DEU', 'RUS', 'ESP'],
-      marker: {
+var data = [{
+    type: 'scattergeo',
+    mode: 'markers',
+    locations: ['FRA', 'DEU', 'RUS', 'ESP'],
+    marker: {
         size: [20, 30, 15, 10],
         color: [10, 20, 40, 50],
         cmin: 0,
         cmax: 50,
         colorscale: 'Greens',
         colorbar: {
-          title: 'Some rate',
-          ticksuffix: '%',
-          showticksuffix: 'last',
+            title: 'Some rate',
+            ticksuffix: '%',
+            showticksuffix: 'last'
         },
         line: {
-          color: 'black',
-        },
-      },
-      name: 'europe data',
+            color: 'black'
+        }
     },
-  ],
-  {
-    geo: {
-      scope: 'europe',
-      resolution: 50,
-      projection: {
-        type: 'mercator',
-        scale: 5,
-      },
-      center: { lon: 2.2137, lat: 46.2276 },
-    },
-    width: 800,
-    height: 600,
-  },
-);
+    name: 'europe data'
+}];
 
-Plotly.newPlot(
-  'chart5',
-  [
-    {
-      type: 'scattergeo',
-      mode: 'markers',
-      locations: ['FRA', 'DEU', 'RUS', 'ESP'],
-      marker: {
-        size: [20, 30, 15, 10],
-        color: [10, 20, 40, 50],
-        cmin: 0,
-        cmax: 50,
-        colorscale: 'Greens',
-        colorbar: {
-          title: 'Some rate',
-          ticksuffix: '%',
-          showticksuffix: 'last',
-        },
-        line: {
-          color: 'black',
-        },
-      },
-      name: 'europe data',
-    },
-  ],
-  {
+var layout = {
     geo: {
-      scope: 'europe',
-      resolution: 50,
-      projection: {
-        type: 'mercator',
-        scale: 5,
-      },
-      center: { lon: 2.2137, lat: 46.2276 },
+        scope: 'europe',
+        resolution: 50,
+        projection: {
+            type: 'mercator',
+            scale: 5  
+        },
+        center: { lon: 2.2137, lat: 46.2276 }
     },
     width: 800,
-    height: 600,
-  },
-);
+    height: 600 
+};
+
+Plotly.newPlot('chart4',
+    [
+      {
+        type: 'scattergeo',
+        mode: 'markers',
+        locations: ['FRA', 'DEU', 'RUS', 'ESP'],
+        marker: {
+          size: [20, 30, 15, 10],
+          color: [10, 20, 40, 50],
+          cmin: 0,
+          cmax: 50,
+          colorscale: 'Greens',
+          colorbar: {
+            title: 'Some rate',
+            ticksuffix: '%',
+            showticksuffix: 'last',
+          },
+          line: {
+            color: 'black',
+          },
+        },
+        name: 'europe data',
+      },
+    ],
+    {
+      geo: {
+        scope: 'europe',
+        resolution: 50,
+        projection: {
+          type: 'mercator',
+          scale: 5,
+        },
+        center: { lon: 2.2137, lat: 46.2276 },
+      },
+      width: 800,
+      height: 600,
+    },
+  );
+  
+  Plotly.newPlot(
+    'chart5',
+    [
+      {
+        type: 'scattergeo',
+        mode: 'markers',
+        locations: ['FRA', 'DEU', 'RUS', 'ESP'],
+        marker: {
+          size: [20, 30, 15, 10],
+          color: [10, 20, 40, 50],
+          cmin: 0,
+          cmax: 50,
+          colorscale: 'Greens',
+          colorbar: {
+            title: 'Some rate',
+            ticksuffix: '%',
+            showticksuffix: 'last',
+          },
+          line: {
+            color: 'black',
+          },
+        },
+        name: 'europe data',
+      },
+    ],
+    {
+      geo: {
+        scope: 'europe',
+        resolution: 50,
+        projection: {
+          type: 'mercator',
+          scale: 5,
+        },
+        center: { lon: 2.2137, lat: 46.2276 },
+      },
+      width: 800,
+      height: 600,
+    },
+  );
+  
